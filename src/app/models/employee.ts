c3c9f4cc09af6f69c8c658e30203aee610bd9d70
Nagesha <nageshasriram@gmail.com>
2019-07-31 15:58:24 +0530

    new changesf

---
+++ src/app/models/employee.ts
@@ -0,0 +1,6 @@
+export interface Employee {
+    empId:string,
+    name:string,
+    designation:string,
+    salary:string
+}
---
+++ src/app/service/http-client.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Employee } from '../models/employee';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpClientService {
+
+  constructor(private httpClient:HttpClient) { }
+
+  getEmployees()
+  {
+    console.log("test call");
+    return this.httpClient.get<Employee[]>('http://localhost:8080/employees');
+  }
+}
